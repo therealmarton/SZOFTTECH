d7fabe47c39e70999985bfa910c4ed37bce14ceb
therealmarton <28adammarton28@gmail.com>
2024-02-22 11:12:39 +0100

    GG

--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -19,7 +19,32 @@ namespace WinFormsApp2
         {
             if (n == 0) return 0;
             if (n == 1) return 1;
-            return Fibonacci(n - 1) + Fibonacci(n-2);
+            return Fibonacci (n - 1) + Fibonacci(n - 2);
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            List<Elem> elemek = new List<Elem>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Button b = new Button();
+                b.Top = i * 30;
+                b.Text = Fibonacci(i).ToString();
+                Controls.Add(b);
+                Elem ujElem = new Elem();
+                ujElem.Sorszam = i;
+                ujElem.Ertek = i * 30;
+                elemek.Add(ujElem);
+            }
+            dataGridView1.DataSource = elemek;
+
+        }
+        class Elem
+        {
+            public int Sorszam {get;set;}  
+            public long Ertek { get; set; }
         }
 
     }
